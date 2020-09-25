e2c1d7eceba0914b7029f1e169ea21a6a3846c3d
Alfred <appiagyeialfred815@gmail.com>
2020-09-25 14:26:23 +0000

    display forms login page

--- App.js
+++ App.js
@@ -1,21 +1,106 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.loginContainer}>
+        <Text style={styles.loginText}>Log in</Text>
+      </View>
+
+
+
+      <View>
+        <TextInput placeholder="Username"
+         style={styles.inputs}
+         placeholderTextColor="#aaaaaa"
+          />
+
+        <TextInput placeholder="password" 
+         style={styles.inputs}
+         placeholderTextColor="#aaaaaa"
+         secureTextEntry={true}
+          />
+        <Text style={styles.forgotpassword} >Forgot password?</Text>
+      </View>
+
+
+
+      <View>
+         <TouchableOpacity style={styles.loginButtonContainer}>
+           <Text style={styles.loginButtonText}>Log in</Text>
+         </TouchableOpacity>
+      </View>
+
+
+      <View style={styles.donthaveAccountContainer}>
+        <Text style={styles.donthaveAccountText}>Dont have an account?</Text>
+        <Text style={styles.signupText}>Sign up</Text>
+      </View>
+        
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+     marginHorizontal:50,
+     marginTop:100
   },
-});
+
+  loginContainer:{
+        marginBottom:60
+  },
+
+  loginText:{
+    fontSize:50,
+    color:"#6a5acd"
+  },
+
+  inputs:{
+     height:60,
+     borderBottomWidth:2,
+     borderBottomColor:"#6a5acd",
+     marginTop:30
+  },
+
+  forgotpassword:{
+    alignSelf:"flex-end",
+    marginTop:3,
+    color:"deepskyblue",
+    fontSize:16
+  },
+
+  loginButtonContainer:{
+    height:50,
+    backgroundColor:"#6a5acd",
+    alignItems:"center",
+    justifyContent:"center",
+    borderRadius:15,
+    marginVertical:55
+  },
+
+  loginButtonText:{
+      color:"white",
+      fontSize:22,
+      fontWeight:"bold",
+  },
+
+  donthaveAccountContainer:{
+    flexDirection:"row",
+    justifyContent:"center"
+  },
+
+  donthaveAccountText:{
+    marginHorizontal:6,
+    fontSize:16
+  },
+
+  signupText:{
+        fontSize:16,
+        color:"#6a5acd"
+  }
+  
+ });
